d9bc01dab81132b4f5fd71c25cc2e66fa545d0c1
Harshith Alfred <harshithalfred@gmail.com>
2024-07-18 09:12:46 +0530

    Update top.jsx

--- src/top.jsx
+++ src/top.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function head(){
     return (
         <>
-        <h1 className='base'>Data Base</h1>
+        <h1 className='base'>Factory Data Base</h1>
         <marquee> <h3>####     Jagdish U Aradhya 67 #### Mohammad Kaif 94 #### Mahesh R 85 #### Harshith Alfred 193 </h3> </marquee>
         </>
     
